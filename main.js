2d2b92585607e4c4df81ef991528ed94ba8f74cb
Jordan Epps <jordanepps@gmail.com>
2019-01-03 19:48:03 -0500

    Add some styling to images

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function createImageTag(imageSrc) {
-	return `<img src="${imageSrc}" alt="random image of a dog"/>`;
+	return `<img class="dog-image" src="${imageSrc}" alt="random image of a dog"/>`;
 }
 
 function loadDogImages(data) {
@@ -10,7 +10,7 @@ function loadDogImages(data) {
 
 function getDogImages(number) {
 	console.log(number);
-	fetch(`https://dog.ceo/api/breeds/image/random/${number}`)
+	fetch(`https://dog.ceo/api/breeds/image/random/${number || 3}`)
 		.then(res => res.json())
 		.then(resJSON => loadDogImages(resJSON))
 		.catch(err => console.log(err));
