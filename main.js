ab4d9ca278976836dce53b7454a218eea43ecf00
Jordan Epps <jordanepps@gmail.com>
2019-01-03 21:25:26 -0500

    Add feature to search for specific breed

--- main.js
+++ main.js
@@ -1,31 +1,50 @@
 function createImageTag(imageSrc) {
-	return `<img class="dog-image" src="${imageSrc}" alt="random image of a dog"/>`;
+    return `<img class="dog-image" src="${imageSrc}" alt="random image of a dog"/>`;
 }
 
 function loadDogImages(data) {
-	console.log(data.message);
-	// $('#image-container').html('<h2>TEST</h2>');
-	$('#image-container').html(data.message.map(createImageTag));
+    $('#image-container').html(data.message.map(createImageTag));
+}
+
+function loadOneDogImage(data) {
+    console.log(data);
+    $('#image-container').html(createImageTag(data.message));
+}
+
+function loadErrorMessage(message) {
+    $('#image-container').html(`<h2>${message}</h2>`);
 }
 
 function getDogImages(number) {
-	console.log(number);
-	fetch(`https://dog.ceo/api/breeds/image/random/${number || 3}`)
-		.then(res => res.json())
-		.then(resJSON => loadDogImages(resJSON))
-		.catch(err => console.log(err));
+    fetch(`https://dog.ceo/api/breeds/image/random/${number}`)
+        .then(res => res.json())
+        .then(resJSON => loadDogImages(resJSON))
+        .catch(err => console.log(err));
 }
 
-function watchForm() {
-	$('#js-dog-form').submit(e => {
-		e.preventDefault();
-		console.log('clicked');
-		getDogImages($('#number').val());
-	});
+function getDogBreedImage(breed) {
+    fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+        .then(res => res.json())
+        .then(resJSON => {
+            resJSON.status === 'error' ? loadErrorMessage(resJSON.message) : loadOneDogImage(resJSON);
+        })
+        .catch(err => console.log(err));
+}
+
+function watchForms() {
+    $('#js-random-dog-form').submit(e => {
+        e.preventDefault();
+        let imgCount = $('#number').val() === '' ? 3 : $('#number').val();
+        getDogImages(imgCount);
+    });
+    $('#js-breed-form').submit(e => {
+        e.preventDefault();
+        getDogBreedImage($('#breed').val());
+    })
 }
 
 function loadDogApp() {
-	watchForm();
+    watchForms();
 }
 
 $(loadDogApp);
